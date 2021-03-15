155702deda5f431f53522804bbe9826f72d21c18
Danny <danny.brogno@gmail.com>
2021-03-15 18:40:09 +0100

    edit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
+// Il computer deve generare 16 numeri casuali tra 1 e 100.
+// I numeri non possono essere duplicati.
+// In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+// L’utente non può inserire più volte lo stesso numero.
+// Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
+// La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+// BONUS: (da fare solo se funziona tutto il resto)
+// all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
+// con difficoltà 0 => tra 1 e 100
+// con difficoltà 1 => tra 1 e 80
+// con difficoltà 2 => tra 1 e 50
+// Consigli del giorno: :party_wizard:
+// Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
+// Ad esempio:
+// Di cosa ho bisogno per generare i numeri?
+// Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
+// Proviamo prima con pochi numeri, inserire 86 numeri ogni volta potrebbe essere un po’ scocciante :occhiolino:
+// Le validazioni e i controlli possiamo farli anche in un secondo momento.
+// Ricordatevi che se non sappiamo quante volte dobbiamo fare una cosa ci serve…
+
+
+
 // Recap funzioni
 // 1 = cosa sono
 // 2 = quando servono
@@ -35,9 +58,9 @@ function divider (nominatore, denominatore) {
 
 // console.log("prima della funzione");
 
-var risultato = subtraction(100, 31);
-var risultato1 = sum(60, 9);
-var risultato2 = multiplier(7, 10);
-var risultato3 = divider(70, 7);
+var menus = subtraction(100, 31);
+var plus = sum(60, 9);
+var times = multiplier(7, 10);
+var dividedBy = divider(70, 7);
 
 // console.log("dopo la funzione");
